f12e24541605c91eb71ed63132be99ede43e1430
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2023-05-23 21:21:29 +0530

    added login and signup pages

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from 'react-router-dom'
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="myCard">
+      <div class="card auth-card">
+        <h2>Instagram</h2>
+        <input type="text" placeholder="E-mail" />
+        <input type="password" placeholder="Password" />
+        <button
+          class="btn waves-effect waves-light #64b5f6 blue lighten-2"
+          
+        >
+          Login
+         
+        </button>
+        <h6>
+          <Link to='/Signup'>Don't have an account?</Link>
+        </h6>
+      </div>
+    </div>
+  );
 }
--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div className="myCard">
+    <div class="card auth-card">
+      <h2>Instagram</h2>
+      <input type="text" placeholder="E-mail" />
+      <input type="text" placeholder="Name" />
+      <input type="password" placeholder="Password" />
+      <input type="password" placeholder="Confirm Password" />
+      <button
+        class="btn waves-effect waves-light #64b5f6 blue lighten-2"
+        
+      >
+        SignUp
+       
+      </button>
+      <h6>
+          <Link to='/signin'>Already have an account?</Link>
+        </h6>
+    </div>
+  </div>
   )
 }
