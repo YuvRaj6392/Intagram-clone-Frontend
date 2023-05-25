a25181cedc8f0dbc0b3a2176e3d51be4f677abdb
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2023-05-25 21:50:38 +0530

    added ui changes to home.js

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,6 +2,56 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className='card home-card'>
+            <h5>Yuv Raj</h5>
+            <div className='card-image'>
+              <img src="https://images.unsplash.com/photo-1505832018823-50331d70d237?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHdhbGxwYXBlciUyMDRrfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="cardImage" />
+            </div>
+            <div className='card-content'>
+            <i class="material-icons" style={{color:'red'}}>favorite</i>
+                <h6>Title</h6>
+                <p>This is an amazing scenic post!</p>
+                <input type="text" placeholder='add a comment' />
+            </div>
+        </div>
+        <div className='card home-card'>
+            <h5>Yuv Raj</h5>
+            <div className='card-image'>
+              <img src="https://images.unsplash.com/photo-1505832018823-50331d70d237?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHdhbGxwYXBlciUyMDRrfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="cardImage" />
+            </div>
+            <div className='card-content'>
+            <i class="material-icons" style={{color:'red'}}>favorite</i>
+                <h6>Title</h6>
+                <p>This is an amazing scenic post!</p>
+                <input type="text" placeholder='add a comment' />
+            </div>
+        </div>
+        <div className='card home-card'>
+            <h5>Yuv Raj</h5>
+            <div className='card-image'>
+              <img src="https://images.unsplash.com/photo-1505832018823-50331d70d237?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHdhbGxwYXBlciUyMDRrfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="cardImage" />
+            </div>
+            <div className='card-content'>
+            <i class="material-icons" style={{color:'red'}}>favorite</i>
+                <h6>Title</h6>
+                <p>This is an amazing scenic post!</p>
+                <input type="text" placeholder='add a comment' />
+            </div>
+        </div>
+        <div className='card home-card'>
+            <h5>Yuv Raj</h5>
+            <div className='card-image'>
+              <img src="https://images.unsplash.com/photo-1505832018823-50331d70d237?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHdhbGxwYXBlciUyMDRrfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="cardImage" />
+            </div>
+            <div className='card-content'>
+            <i class="material-icons" style={{color:'red'}}>favorite</i>
+                <h6>Title</h6>
+                <p>This is an amazing scenic post!</p>
+                <input type="text" placeholder='add a comment' />
+            </div>
+        </div>
+    </div>
   )
 }
+//card-image, card-content is already defined in materialize
