262dcdc812dfccd32d795d218b9c0f968e1c5dad
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2023-06-09 23:32:04 +0530

    added a logout functinality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { UserContext } from '../App'
 
 export default function Navbar() {
+const history=useNavigate()
 const {state,dispatch}=useContext(UserContext)
 //state has the details of the user!
 const renderList=()=>{
@@ -11,14 +12,23 @@ const renderList=()=>{
   {
     return [
       <li><Link to="/profile">Profile</Link></li>,
-        <li><Link to="/create">Create Post</Link></li>
+        <li><Link to="/create">Create Post</Link></li>,
+        <li><button
+      className="btn waves-effect waves-light #64b5f6 red darken-2" onClick={()=>{
+        localStorage.clear();
+        dispatch({type:"CLEAR"})
+        history('/signin');
+      }}>
+      Logout
+    </button></li>
     ]
   }
   else
   {
     return [
       <li><Link to="/signin">Login</Link></li>,
-      <li><Link to="/signup">Signup</Link></li>
+      <li><Link to="/signup">Signup</Link></li>,
+      
     ]
   }
 }
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -4,11 +4,13 @@ export const reducer=(state,action)=>{
     {
         return action.payload
     }
-    else
+    if(action.type==="CLEAR")
     {
-        return state
+        return null
     }
+    return state
 }
 //action describes the intention of the work(what you need to do)
 //dispatch acts as a medium and sends action to the reducer
 //reducer determines the change. Return the new state and tells the store what to do
+//line number 9- state will become null!1
