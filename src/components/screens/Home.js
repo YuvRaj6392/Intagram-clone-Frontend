83957092f0f36077f3bb3e8a65b6190efe1d3a60
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2023-06-23 16:14:47 +0530

    started the implementation of comment

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,7 +2,8 @@ import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../App';
 export default function Home() {
   const [data,setData]=useState("");
-  const {state,dispatch}=useContext(UserContext)
+  const {state,dispatch}=useContext(UserContext);
+  const {comment,setComment}=useState("")
   useEffect(()=>{
   fetch('http://localhost:8080/api/showallposts',{
     headers:{
@@ -74,6 +75,36 @@ export default function Home() {
       setData(newData)
     })
   }
+
+  const makeComment=(text,postId)=>{
+    fetch('http://localhost:8080/api/comments',{
+      method:'Put',
+      headers:{
+        'Content-Type':'application/json',
+        'x-access-token':localStorage.getItem('token')
+      },
+      body:JSON.stringify({
+        text:text,
+        postId:postId
+      })
+    }).then(res=>res.json())
+    .then(result=>{
+      console.log(result)
+      const newData=data.map(item=>{
+        if(item._id===result.message._id)
+        {
+          return result.message;
+        }
+        else
+        {
+          return item;
+        }
+      })
+      setData(newData)
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
   return (
     <div className='home'>
     {
@@ -85,22 +116,35 @@ export default function Home() {
               <img src={item.photo} alt={item.postedBy.name} />
             </div>
             <div className='card-content'>
-            <i className="material-icons" style={{color:'red'}}>favorite</i>
+           
             {item.likes.includes(state._id)
             ?
-            <i className="material-icons" onClick={()=>{
+            <i className="material-icons" style={{color:'red',cursor:'pointer'}}  onClick={()=>{
               unlikePost(item._id)
-            }}>thumb_down</i>
-            :
-            <i className="material-icons" onClick={()=>{
+            }}>favorite</i>
+           :
+           <i className="material-icons" style={{cursor:'pointer'}}  onClick={()=>{
               likePost(item._id)
-            }}>thumb_up</i>
+            }}>favorite_border</i>
            
             }
                 <h6>{item.likes.length} liked</h6>
                 <h6>{item.title}</h6>
                 <p>{item.body}</p>
+                {
+                  item.comments.map(record=>{
+                    return (
+                      <h6><span style={{fontWeight:'500'}}>{record.postedBy.name}</span>&nbsp;{record.text}</h6>
+                    )
+                  })
+                }
+                <form onSubmit={e=>{
+                  e.preventDefault();
+                  makeComment(e.target[0].value,item._id)
+                }} >
                 <input type="text" placeholder='add a comment' />
+                </form>
+                
             </div>
         </div>
         )
